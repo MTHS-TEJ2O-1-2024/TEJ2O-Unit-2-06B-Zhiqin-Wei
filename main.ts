7a618ef73bda5930da9483c367daddcd0e31bdac
Zhiqin-Wei <zhiqin.daly.wei@mths.ca>
2024-10-06 18:14:10 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Daly
+ * Created on: Sep 2024
  * This program ...
 */
 
-basic.showString('Hello, World!')
+//setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+pins.digitalWritePin(DigitalPin.P16, 0)
+
+//ture on LED
+input.onButtonPressed(Button.A, function() {
+  basic.showIcon(IconNames.Yes)
+  pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+//turn off LED
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
