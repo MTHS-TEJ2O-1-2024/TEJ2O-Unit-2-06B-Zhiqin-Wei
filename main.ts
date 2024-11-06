c5f3e44d5ec1d9e5ada183a3ab6ad51e3a03d7aa
Zhiqin-Wei <zhiqin.daly.wei@mths.ca>
2024-11-06 10:56:40 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: Daly
- * Created on: Sep 2024
- * This program ...
+*
+* Created by: Daly Wei
+* Created on: Sep 2024
+* This program make the LED up
 */
 
 //setup
@@ -11,9 +11,9 @@ basic.showIcon(IconNames.Happy)
 pins.digitalWritePin(DigitalPin.P16, 0)
 
 //ture on LED
-input.onButtonPressed(Button.A, function() {
-  basic.showIcon(IconNames.Yes)
-  pins.digitalWritePin(DigitalPin.P16, 1)
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P16, 1)
 })
 
 //turn off LED
